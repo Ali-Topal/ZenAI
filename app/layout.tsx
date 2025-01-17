5645cdbf3b7c33b3111bd1d208f089cb053c6807
Ali Topal <zmac634@live.rhul.ac.uk>
2025-01-17 21:25:31 +0000

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import { WalletContextProvider } from '../utils/wallet';
+import { WalletContextProvider } from './utils/wallet';
 import './globals.css';
 
 export default function RootLayout({
